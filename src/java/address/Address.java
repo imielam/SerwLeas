85d208b0fce5215e977d5a2731622c799e5aec87
macimi <macimi@wp.pl>
2013-05-12 21:57:10 +0200

    update packages name

--- src/java/address/Address.java
+++ src/java/address/Address.java
@@ -2,12 +2,12 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package company;
+package address;
 
 /**
  *
  * @author Macimi
  */
-public class Company {
+public class Address {
     
 }
